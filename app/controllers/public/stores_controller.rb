b903c2c82f59b599e28040aa5cbaec7aa06776c3
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-21 06:40:32 +0000

    create:store_controller

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -2,31 +2,51 @@ class Public::StoresController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    
+    @shop = Shop.new
   end
-  
+
   def create
-    
+    @shop = current_user.shops.new(shop_params)
+    if @shop.save
+      flash[:notice] = "投稿しました"
+      redirect_to shops_path
+    else
+      render :new
+    end
   end
-  
+
   def index
-    
+    @shops = Shop.all
   end
-  
+
   def show
-    
+    @shop = Shop.find(params[:id])
   end
-  
+
   def edit
-    
+    @shop = Shop.find(params[:id])
   end
-  
+
   def update
-    
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      flash[:notice] = "更新しました"
+      redirect_to shops_path
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
-    
+    shop = Shop.find(params[:id])
+    shop.destroy
+    redirect_to shops_path
   end
-  
+
+  private
+
+  def shop_params
+    params.require(:store).permit(:name, :description, :address, :store_image)
+  end
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,18 @@
 class Store < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true, length: {maximum: 200}
+  
+  belongs_to :user
+  
+  has_one_attached :store_image
+  
+  
+  def get_store_image(width,height)
+    unless store_image.attached?
+      file_path = Rails.root.join('app/assets/images/store_no_image.jpg')
+      store_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')      
+    end
+    store_image.variant(resize_to_limit: [width,height]).processed
+  end
+  
 end
