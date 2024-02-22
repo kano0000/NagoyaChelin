1361e549523269a9875b72dedca2cc28e7f27a2a
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-22 06:46:59 +0000

    create:user.store_edit

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,19 +1,31 @@
 class Public::UsersController < ApplicationController
-  
+
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
-    
+    @user = User.find(params[:id])
   end
-  
+
   def update
-    
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "更新しました"
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
 end
