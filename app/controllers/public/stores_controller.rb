7f56136637c5b1d31c2417eb4455f179d55d70f4
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-02-27 05:15:25 +0000

    update:layout

--- app/controllers/public/stores_controller.rb
+++ app/controllers/public/stores_controller.rb
@@ -1,12 +1,12 @@
 class Public::StoresController < ApplicationController
   before_action :authenticate_user!
-  
+
   def hashtag
     @user = current_user
     @hashtag = Hashtag.find_by(hashname: params[:name])
     @stores = @hashtag.stores.all.reverse_order
   end
-  
+
   def new
     @store = Store.new
   end
@@ -29,6 +29,7 @@ class Public::StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     @store_comment = StoreComment.new
+    @stores = Store.where("address = ? OR name = ?", @store.address, @store.name)
   end
 
   def edit
