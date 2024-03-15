39aef888c792fb06e77f5dba2e4229be9842dcae
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-03-15 11:40:55 +0000

    fix:header

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,5 @@
 class Public::HomesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :top
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
