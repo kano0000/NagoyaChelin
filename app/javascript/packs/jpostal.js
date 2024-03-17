ddd805012601f4ecd92e3726c0170bf62db50978
ChisatoKano <129806087+kano0000@users.noreply.github.com>
2024-03-17 05:51:49 +0000

    update:jpostal

--- app/javascript/packs/jpostal.js
+++ app/javascript/packs/jpostal.js
@@ -1,20 +1,4 @@
 import "jquery-jpostal-ja";
-// import $ from "jquery"; // jQueryがインポートされていることを確認
-
-// function jpostal() {
-//   console.log("jpostal function called");
-//   $('#zipcode').jpostal({
-//     postcode: ['#zipcode'],
-//     address: {
-//       '#store_address': '%3%4%5'
-//     }
-//   });
-// }
-
-// $(document).on("turbolinks:load", function() {
-//   console.log("Turbolinks load event fired");
-//   jpostal();
-// });
 
 function jpostal() {
   $('#zipcode').jpostal({   
